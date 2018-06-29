454bd531fb867ffaaf48c99748fe338f23e84908
Jonas Aklin <jonas.aklin@gmail.com>
2018-06-30 00:44:45 +0200

    StickFigurePrototype project added

---
+++ StickFigurePrototype/Assets/Scripts/FollowCameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float targetZ = -10;
+    public float speedModifier = 1;
+    public Vector2 positionOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if (this.followTarget == null)
+        {
+            return;
+        }
+
+        var targetPosition = new Vector3(
+            this.followTarget.transform.position.x + this.positionOffset.x,
+            this.followTarget.transform.position.y + this.positionOffset.y,
+            this.targetZ);
+
+        var newPosition = Vector3.Lerp(this.transform.position, targetPosition, speedModifier * Time.deltaTime);
+
+        this.transform.position = newPosition;
+	}
+}
---
+++ StickFigurePrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 5;
+
+    private float deadThreshold = 0.2f;
+    private Rigidbody2D playerBody;
+
+	// Use this for initialization
+	void Start()
+    {
+        this.playerBody = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        var moveAxis = Input.GetAxisRaw("Horizontal");
+        if (Math.Abs(moveAxis) > this.deadThreshold)
+        {
+            var velocityX = moveAxis * this.playerSpeed;
+
+            this.playerBody.velocity = new Vector2(
+                velocityX,
+                this.playerBody.velocity.y);
+        }
+	}
+}
